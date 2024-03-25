d07ffecce12d99f0b9e8946f3ceec7c98d4752c6
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-03-24 21:08:23 -0300

    Proyecto finalizado

---
+++ src/logica/PlanillaDigital.java
@@ -0,0 +1,14 @@
+package logica;
+import igu.Screen;
+
+public class PlanillaDigital {
+
+    public static void main(String[] args) {
+        //Creo y muestro mi pantalla
+        Screen pantalla = new Screen(); 
+        pantalla.setVisible(true); 
+        pantalla.setLocationRelativeTo(null); 
+        
+    }
+    
+}
